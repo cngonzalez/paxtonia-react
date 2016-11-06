aa2b07d20e7a291232c66593643f04a06cf36c2a
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-06 08:18:56 -0500

    added routes

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Route, IndexRoute } from 'react-router';
 import App from './src/App'
-import Splash from './src/components/Splash'
+import SplashContainer from './src/components/containers/SplashContainer'
 import Paxtonia from './src/components/Paxtonia'
 import Results from './src/components/Results'
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Splash}/>
+    <IndexRoute component={SplashContainer}/>
     <Route path="/game" component={Paxtonia} />
     <Route path="/completed" component={Results} />
    </Route> 
